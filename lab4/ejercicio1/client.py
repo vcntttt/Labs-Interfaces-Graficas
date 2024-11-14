e12a3aa9c106ca6e867f2befdd34e354cf05f871
Vicente Rivera <vicenterivera225@gmail.com>
2024-11-14 15:31:54 -0300

    era cada 3 segundos xd

--- lab4/ejercicio1/client.py
+++ lab4/ejercicio1/client.py
@@ -2,14 +2,6 @@ import time
 import os
 import requests
 
-# while True:
-#   try: 
-#     print("hola")
-#     time.sleep(1)
-#   except KeyboardInterrupt:
-#     print("programa detenido")
-#     break
-
 def sendIMG(img):
     print(f"Enviando imagen {img}")
     url = "http://127.0.0.1:5000/img/upload"
@@ -21,4 +13,4 @@ imgPath = "ejercicio1/IMG"
 files = os.listdir(imgPath)
 for file in files:
     sendIMG(f"{imgPath}/{file}")
-    time.sleep(1)
+    time.sleep(3)
